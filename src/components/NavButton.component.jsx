e7dab6ce769ce1a4c55c34a0005b29559565fc88
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-10 17:25:28 -0300

    Kodchasan font for nav buttons

--- src/components/NavButton.component.jsx
+++ src/components/NavButton.component.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function NavButton({ path, name }) {
   return (
-    <li className="flex items-center justify-center text-4xl font-semibold">
+    <li className="flex items-center justify-center font-Kodchasan text-3xl font-semibold">
       <Link to={path}>{name}</Link>
     </li>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      Kodchasan: ["Kodchasan", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
