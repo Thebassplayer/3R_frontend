0c2b58a3bd7d8ab0e19c3df79af9c83bebf38499
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-12 20:50:11 -0300

    create section container component

---
+++ src/components/SectionContainer.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SectionContainer() {
+  return (
+    <div className="h-screen bg-gradient-to-b from-[#3A2CDA] to-black"></div>
+  );
+}
+
+export default SectionContainer;
