eda1f0b53c1b454d24d5f3cf4f91a505828204ba
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-13 19:52:30 -0300

    responsive nav design implementation

--- src/components/Nav.component.jsx
+++ src/components/Nav.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import _3R_logo from "../assets/logo_web.svg";
 import NavButton from "./NavButton.component";
+import Hamburguer from "../assets/hamburguer_menu.svg";
 
 const navButtons = [
   {
@@ -23,13 +23,16 @@ const Nav = Component =>
   function HOC() {
     return (
       <div className="h-screen bg-gradient-to-b from-[#3A2CDA] to-black">
-        <div className="mx-4 flex justify-between">
-          <img className="w-28" src={_3R_logo} alt="3R_logo" />
-          <ul className="mr-4 flex gap-16">
-            {navButtons.map(button => (
-              <NavButton key={button.name} {...button} />
-            ))}
-          </ul>
+        <div className="flex justify-between p-3 sm:px-6">
+          <img className="w-12 sm:w-20 md:w-28" src={_3R_logo} alt="3R_logo" />
+          <div className="flex items-center">
+            <img src={Hamburguer} className="sm:w-12 md:hidden" />
+            <ul className="mr-4 hidden gap-16 md:flex">
+              {navButtons.map(button => (
+                <NavButton key={button.name} {...button} />
+              ))}
+            </ul>
+          </div>
         </div>
         <Component />
       </div>
