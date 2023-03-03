f455d0c5bfe5ac186ebab7904fa28cf27fc91060
Roy Lopez <roylopezdev@gmail.com>
2023-03-03 15:50:45 -0300

    margin update

--- src/components/HomeHero.component.jsx
+++ src/components/HomeHero.component.jsx
@@ -3,14 +3,14 @@ import hero_ilustration_shadow from "../assets/hero_ilustration_shadow.svg";
 
 function HomeHero() {
   return (
-    <div className="mt-8 flex flex-col items-center justify-center">
+    <div className="mt-4 flex flex-col items-center justify-center">
       <div className="w-10/12">
-        <h1 className="text-4xl font-extrabold">
+        <h1 className="text-center text-3xl font-extrabold">
           Desarrollamos su presencia en línea{" "}
         </h1>
       </div>
       <img
-        className="mt-24 block scale-95 
+        className="mt-4 block scale-95 
         transition-all
         duration-500 ease-in-out hover:scale-100 hover:transition-all hover:duration-500 hover:ease-in-out"
         src={hero_ilustration_shadow}
